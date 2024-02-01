3abb0598a9ab53271400c907fbaf90133935286e
jarsing <67315337+apwla@users.noreply.github.com>
2024-02-01 19:48:13 +0800

    通过java代码设置JVM时区

--- campus-modular/src/main/java/com/oddfar/campus/CampusApplication.java
+++ campus-modular/src/main/java/com/oddfar/campus/CampusApplication.java
@@ -15,6 +15,9 @@ public class CampusApplication {
 
     public static void main(String[] args) {
         //设置+8时区，避免因为时区问题导致预约时间不正确
+        // Spring Boot应用启动的时候，设置Java虚拟机（JVM）的默认时区;
+        // 它通过TimeZone.getTimeZone(ZoneOffset.of("+8"))创建了一个表示UTC+8时区的TimeZone对象。ZoneOffset.of("+8")是基于Java 8引入的java.time包的API，用于表示从UTC时区偏移+8小时的时区偏移量。
+        // 然后，这个时区对象被设置为整个Java虚拟机的默认时区
         TimeZone.setDefault(TimeZone.getTimeZone(ZoneOffset.of("+8")));
         SpringApplication.run(CampusApplication.class, args);
     }
